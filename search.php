af3a2272a694f92b76e6889d26f853ce19edefde
ivanbadun <ivan.ba@thewhitelabelagency.com>
2026-02-13 13:57:25 +0200

    do errors

--- search.php
+++ search.php
@@ -7,26 +7,28 @@
 get_header(); ?>
 
 <div class="grid-container">
-    <div class="grid-x grid-margin-x posts-list">
-        <div class="cell small-12">
-            <!-- BEGIN of search results -->
-            <main class="main-content">
-                <h1 class="page-title"><?php printf(__('Search Results for: %s', 'fwp'), '<span>' . esc_html(get_search_query()) . '</span>'); ?></h1>
-                <?php get_search_form(); ?>
-                <?php if (have_posts()) { ?>
-                    <?php while (have_posts()) {
-                        the_post(); ?>
-                        <?php get_template_part('parts/loop', 'post'); // Post item?>
-                    <?php } ?>
-                <?php } else { ?>
-                    <p><?php _e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'fwp'); ?></p>
-                <?php } ?>
-                <!-- BEGIN of pagination -->
+    <div class="main-content">
+        <h1 class="page-title">
+            <?php printf(__('Search Results for: %s', 'fwp'), '<span>' . esc_html(get_search_query()) . '</span>'); ?>
+        </h1>
+        <?php get_search_form(); ?>
+
+        <?php if (have_posts()) : ?>
+            <div class="grid-x grid-margin-x posts-list">
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="cell large-4 medium-6 small-12">
+                        <?php get_template_part('parts/loop', 'post'); ?>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+
+            <div class="news-dots-container">
                 <?php foundation_pagination(); ?>
-                <!-- END of pagination -->
-            </main>
-        </div>
-        <!-- END of search results -->
+            </div>
+
+        <?php else : ?>
+            <p><?php _e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'fwp'); ?></p>
+        <?php endif; ?>
     </div>
 </div>
 
